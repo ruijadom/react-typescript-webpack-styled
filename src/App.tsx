804a27c6a3a064559fb19c00da44e05586fc0196
ruijadom <ruijadomingues@gmail.com>
2020-09-13 18:08:06 +0100

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Layout from './components/Layout'
+
 const App: React.FC = (props) => {
-  return <div>Webpack is cool!</div>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 };
 
 export default App;
